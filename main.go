abc7e2bb45abf4fb145ab1ed4276032a69e5232e
Micah Pawlicki <mich.pawlicki@gmail.com>
2025-07-11 13:27:47 -0500

    custom handler

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	strBytes := []byte("OK")
+	w.Write(strBytes)
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -8,6 +22,9 @@ func main() {
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	server.ListenAndServe()
 
+	mux.HandleFunc("/healthz", handler)
+	appReqPath := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", appReqPath)
+	server.ListenAndServe()
 }
