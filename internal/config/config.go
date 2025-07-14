f99c15188aecb0693aae68e2490e79b03ed6b033
Micah Pawlicki <mich.pawlicki@gmail.com>
2025-07-14 16:44:27 -0500

    database linking to config + sqlc generation for users

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,12 @@
 package config
 
-import "sync/atomic"
+import (
+	"sync/atomic"
+
+	"github.com/m-pawlicki/chirpy/internal/database"
+)
 
 type APIConfig struct {
 	FileserverHits atomic.Int32
+	DB             *database.Queries
 }
--- main.go
+++ main.go
@@ -1,23 +1,37 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/m-pawlicki/chirpy/internal/config"
+	"github.com/m-pawlicki/chirpy/internal/database"
 	"github.com/m-pawlicki/chirpy/internal/handlers"
 
 	_ "github.com/lib/pq"
 )
 
 func main() {
+	godotenv.Load(".env")
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Printf("Error opening database: %s", err)
+		return
+	}
+
+	dbQueries := database.New(db)
+	apiCfg := &config.APIConfig{DB: dbQueries}
+	cfgHandlers := handlers.NewAPIHandler(apiCfg)
+
 	mux := http.NewServeMux()
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
-
-	apiCfg := &config.APIConfig{}
-	cfgHandlers := handlers.NewAPIHandler(apiCfg)
 
 	mux.HandleFunc("GET /api/healthz", handlers.HealthHandler)
 	mux.HandleFunc("GET /admin/metrics", cfgHandlers.HitHandler)
