7b0738e0a714ab6909298816dfe857f490bdd2ba
Micah Pawlicki <mich.pawlicki@gmail.com>
2025-07-16 13:58:25 -0500

    jwt + tests

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,13 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (string, error) {
 	h, err := bcrypt.GenerateFromPassword([]byte(password), 4)
@@ -16,4 +23,40 @@ func CheckPasswordHash(password, hash string) error {
 		return err
 	}
 	return nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+
+	claims := jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject:   userID.String(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signed, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+
+	return signed, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) { return []byte(tokenSecret), nil })
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("error parsing token: %v", err)
+	}
+
+	claims, ok := token.Claims.(*jwt.RegisteredClaims)
+	if ok && token.Valid {
+		id, err := uuid.Parse(claims.Subject)
+		if err != nil {
+			return uuid.Nil, fmt.Errorf("error parsing uuid: %v", err)
+		}
+		return id, nil
+	}
+	return uuid.Nil, fmt.Errorf("invalid token: %v", err)
 }
---
+++ internal/auth/auth_test.go
@@ -0,0 +1,43 @@
+package auth
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+)
+
+func TestTokenCreation(t *testing.T) {
+	godotenv.Load(".env")
+	testUser := uuid.New()
+	testSecret := os.Getenv("TEST_SECRET")
+	_, err := MakeJWT(testUser, testSecret, time.Second*60)
+	if err != nil {
+		t.Errorf("error making jwt: %v", err)
+	}
+}
+
+func TestTokenValidation(t *testing.T) {
+	godotenv.Load(".env")
+	testUser := uuid.New()
+	testSecret := os.Getenv("TEST_SECRET")
+	jwt, _ := MakeJWT(testUser, testSecret, time.Second*60)
+	_, err := ValidateJWT(jwt, testSecret)
+	if err != nil {
+		t.Errorf("error validating jwt: %v", err)
+	}
+}
+
+func TestTokenExpired(t *testing.T) {
+	godotenv.Load(".env")
+	testUser := uuid.New()
+	testSecret := os.Getenv("TEST_SECRET")
+	jwt, _ := MakeJWT(testUser, testSecret, time.Second*2)
+	time.Sleep(time.Second * 5)
+	_, err := ValidateJWT(jwt, testSecret)
+	if err == nil {
+		t.Errorf("expected expired token error but got nil instead")
+	}
+}
